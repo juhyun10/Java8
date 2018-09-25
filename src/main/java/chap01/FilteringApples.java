faadcadad203eeff6a83f3e4d3db1c2931f7c5ad
juhyun10 <juhyun1010@naver.com>
2018-09-26 01:46:48 +0900

    chap01. Method Reference

---
+++ src/main/java/chap01/FilteringApples.java
@@ -0,0 +1,74 @@
+package chap01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Method Reference (코드 넘겨주기)
+ */
+public class FilteringApples {
+    public static void main(String[] args) {
+        List<Apple> inventory = Arrays.asList(new Apple(80, "green"),
+                                                new Apple(155, "green"),
+                                                new Apple(120, "red"));
+
+        List<Apple> greenApples = filterApples(inventory, FilteringApples::isGreenApple);
+        System.out.println(greenApples);
+
+        List<Apple> headyApples = filterApples(inventory, FilteringApples::isHeavyApple);
+        System.out.println(headyApples);
+    }
+
+    public static boolean isGreenApple(Apple apple) {
+        return "green".equals(apple.getColor());
+    }
+
+    public static boolean isHeavyApple(Apple apple) {
+        return apple.getWeight() > 150;
+    }
+
+    public static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> p) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (p.test(apple)) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    public static class Apple {
+        private int weight = 0;
+        private String color = "";
+
+        public Apple (int weight, String color) {
+            this.weight = weight;
+            this.color = color;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        public String toString() {
+            return "Apple{" +
+                    "color='" + color + '\'' +
+                    ", weight=" + weight +
+                    '}';
+        }
+    }
+}
